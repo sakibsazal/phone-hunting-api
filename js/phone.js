d17ceb8f017013d311d0135c5bc22e35e7eaaee0
sakibsazal <sakibsazal@gmail.com>
2024-03-01 11:01:14 +0600

    added new

---
+++ js/phone.js
@@ -0,0 +1,36 @@
+const loadPhone = async () =>{
+const res = await fetch ('https://openapi.programming-hero.com/api/phones?search=iphone');
+const data = await res.json();
+const phones = data.data;
+displayPhones(phones);
+}
+
+const displayPhones = phones =>{
+    // console.log(phones);
+
+    const phoneContainer = document.getElementById('phone-container');
+
+    phones.forEach(phone => {
+        console.log(phones);
+    // create a div
+    const phoneCard = document.createElement('div');
+    phoneCard.classList = `card w-96 bg-base-500 shadow-xl`;
+    // inner html
+    phoneCard.innerHTML = ` <figure><img src="${phone.image}" alt="Shoes" /></figure>
+    <div class="card-body">
+      <h2 class="card-title">${phone.phone_name}</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
+    </div>
+  </div>`;
+
+//   append child
+phoneContainer.appendChild(phoneCard);
+    });
+}
+
+
+
+loadPhone();
